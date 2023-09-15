d601719fe07a5d1b7efd52451c55e95e0b0e3b19
Mikado <mikadoblaze289>
2023-09-15 22:08:43 +0000

    my commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,22 +5,22 @@
  *   * @n: the number of parameters passed
  *    * Return: Always 0
  *     */
-
 int sum_them_all(const unsigned int n, ...)
 {
-		int sum;
-			unsigned int i;
-				va_list ap;
+	    int sum = 0;
+	        unsigned int i;
+		    va_list ap;
 
-					va_start(ap, n);
+		        va_start(ap, n);
 
-						if (n == 0)
-									return (0);
+			    if (n == 0)
+				            return 0;
 
-							for (i = 0; i < n; i++)
-										sum += va_arg(ap, int);
+			        for (i = 0; i < n; i++)
+					        sum += va_arg(ap, int);
 
-								va_end(ap);
+				    va_end(ap);
 
-									return (sum);
+				        return sum;
 }
+
